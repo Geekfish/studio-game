f6a46300b3ddc9cd8989311c0e9817bf9a5b6214
Eleni Lixourioti <contact@eleni.co>
2021-05-02 21:09:36 +0200

    Progress up to variables and objects

--- studio_game.rb
+++ studio_game.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
-greeting = 'Welcome!'
+players = [
+  { name: 'larry', health: 60 },
+  { name: 'curly', health: 40 },
+  { name: 'moe', health: 58 }
+]
 
-3.times do
-  puts greeting.upcase
+current_time = Time.new
+
+puts 'Player health:'
+players.each do |player|
+  puts "- #{player[:name].capitalize.ljust(30, '.')} #{player[:health]}"
 end
+
+puts "The game started on #{current_time.strftime('%A %d/%m/%Y at %I:%M:%S%p')}"
