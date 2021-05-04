c027ce2604e4ea2b09f42ab55324c47500a5d2f1
Eleni Lixourioti <contact@eleni.co>
2021-05-04 15:17:51 +0200

    Extend tests for Player and configure rubocop for rspec

--- spec/game/player_spec.rb
+++ spec/game/player_spec.rb
@@ -2,9 +2,37 @@ require 'game'
 require 'game/player'
 
 describe Game::Player do
-  it 'sets player name and health' do
-    player = Game::Player.new('bob')
-    expect(player.name).to eq('Bob')
-    expect(player.health).to eq(100)
+  context 'with no player initialized' do
+    it 'sets player name and default health' do
+      player = Game::Player.new('bob')
+      expect(player.name).to eq('Bob')
+      expect(player.health).to eq(100)
+    end
+
+    it 'sets player name and specified health' do
+      player = Game::Player.new('bob', 23)
+      expect(player.name).to eq('Bob')
+      expect(player.health).to eq(23)
+    end
+  end
+  context 'with player already initialized' do
+    def build_player(health)
+      Game::Player.new('bob', health)
+    end
+
+    before do
+      @initial_health = 100
+      @player = build_player(@initial_health)
+    end
+
+    it 'reduces health when blammed' do
+      @player.blam
+      expect(@player.health).to eq(90)
+    end
+
+    it 'replenishes health when w00ted' do
+      @player.w00t
+      expect(@player.health).to eq(115)
+    end
   end
 end
